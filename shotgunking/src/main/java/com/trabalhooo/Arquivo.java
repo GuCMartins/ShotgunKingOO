11bd42ff76e1c92503335eee03d262feb68b7ee8
joaocmfnogueira <joao.nogueira@estudante.ufjf.br>
2022-07-28 17:50:13 -0300

    Adição de da classe Arquivo e JSON, e alteração no pom para GSON

---
+++ shotgunking/src/main/java/com/trabalhooo/Arquivo.java
@@ -0,0 +1,47 @@
+package com.trabalhooo;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Arquivo {
+    public static String lerArquivo(String caminho) throws FileNotFoundException {
+
+        StringBuilder conteudo = new StringBuilder();
+
+        File arquivo = new File(caminho);
+
+        Scanner leitor = new Scanner(arquivo);
+
+        // varrendo o conteúdo do arquivo linha por linha
+        while (leitor.hasNextLine()) {
+            conteudo.append(leitor.nextLine()).append("\n");
+        }
+
+        return conteudo.toString();
+    }
+
+    public static void escreverArquivo(String caminho, String conteudo) {
+        FileWriter fwArquivo;
+        BufferedWriter bwArquivo;
+        try {
+            File arquivo = new File(caminho);
+
+            //Se o arquivo já existir, então abrir para concatenação, caso contrário criar novo arquivo
+            fwArquivo = new FileWriter(arquivo, false);
+            bwArquivo = new BufferedWriter(fwArquivo);
+
+            // escrevendo String no arquivo e adicionando caracter para criar nova linha
+            bwArquivo.write(conteudo + '\n');
+
+            // fechando o arquivo
+            bwArquivo.close();
+            fwArquivo.close();
+
+        } catch (IOException e) {
+            System.err.println("Erro ao tentar escrever no arquivo: " + e.toString());
+        }
+    }
+}
